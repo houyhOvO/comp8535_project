3f9ee6f9d18bed51576e5ee7047eeff9ae9d10d2
houyaohui <u7754892@anu.edu.au>
2024-04-11 23:20:45 +1000

    Updated data_analysis.py

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -11,14 +11,17 @@ print(boston.describe())  # Display statistics of data
 print(boston.info())      # Display a summary of basic information about the data
 
 
-features.append('MEDV')
+# features.append('MEDV')
 correlation = boston.corr()      # Calculate correlation matrix
-# print(correlation)
-fig = plt.figure(figsize=(15, 15))
-ax = fig.add_subplot()           # Add a subplot
-sns.heatmap(correlation, cmap='YlGnBu', vmax=1, vmin=-1, annot=True, annot_kws={"size": 15})  # Generate a heatmap for correlation
-plt.xticks(np.arange(len(features)) + 0.5, features)
-plt.yticks(np.arange(len(features)) + 0.5, features, rotation=0)
-ax.set_title('Correlation coefficient matrix heatmap')
+print(correlation)
+plt.figure(figsize=(15, 15))
+# ax = fig.add_subplot()           # Add a subplot
+sns.heatmap(correlation.iloc[:, 13:], cmap='YlGnBu', linewidths=0.1, annot=True, annot_kws={"size": 20, "weight":"bold"})  # Generate a heatmap for correlation
+# plt.xticks(np.arange(len(features)) + 0.5, features)
+# plt.yticks(np.arange(len(features)) + 0.5, features, rotation=0)
+# plt.xticks(np.arange(13)+0.5, features)
+# plt.yticks(np.arange(13)+0.5, features)
+# ax.set_title('Correlation coefficient matrix heatmap')
+plt.title('Correlation coefficient heatmap', fontsize=20)
 plt.savefig('../figs/heatmap.png')
 plt.show()
