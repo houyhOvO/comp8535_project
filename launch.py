cc012172eeb6a91cc81aeeb740775ca1cedfdd00
Acupoftea <67898965+GUA07@users.noreply.github.com>
2024-05-19 18:33:54 +1000

    Update:Display the linear_regression result in Streamlit

--- launch.py
+++ launch.py
@@ -5,6 +5,13 @@ import numpy as np
 # load the model
 with open('./models/linear_regression.pkl', 'rb') as f:
     model = pickle.load(f)
+with open('./models/scaler.pkl', 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
+try:
+    with open('./models/pca.pkl', 'rb') as pca_file:
+        pca = pickle.load(pca_file)
+except FileNotFoundError:
+    pca = None
 
 st.sidebar.title('**Select Your Configuration Here** 👇')
 st.sidebar.radio("Please select a model to use", ["Linear Regression", "Random Forest", "Decision Tree"])
@@ -22,18 +29,28 @@ with st.expander("What is this app about?"):
 # submit = st.button(label='Get Price')
 
 # 获取用户输入的特征值
-crim = st.number_input('CRIM: per capita crime rate by town')
-zn = st.number_input('ZN: proportion of residential land zoned for lots over 25,000 sq. ft.')
-indus = st.number_input('INDUS: proportion of non-retail business acres per town')
-chas = st.number_input('CHAS: Charles River dummy variable (1 if tract bounds river; 0 otherwise)')
-nox = st.number_input('NOX: nitric oxides concentration (parts per 10 million)')
 rm = st.number_input('RM: average number of rooms per dwelling')
-age = st.number_input('AGE: proportion of owner-occupied units built prior to 1940')
+lstat = st.number_input('LSTAT: lower status of the population(%)')
+ptratio = st.number_input('PTRATIO: pupil-teacher ratio by town')
 
 submit = st.button(label='Get Price')
 
-# 进行预测
-features = np.array([[crim, zn, indus, chas, nox, rm, age]])
-prediction = model.predict(features)
-
-st.write(f"The predicted price is: ${prediction[0]:.2f}")
+# 输出预测结果
+if submit:
+    # 检查所有输入是否有效
+    if rm > 0 and lstat > 0 and ptratio > 0:
+        # 创建一个包含所有特征的数组，用 0 填充未使用的特征
+        features = np.zeros((1, 13))
+        features[0, 5] = rm
+        features[0, 12] = lstat
+        features[0, 10] = ptratio
+        # 标准化输入特征
+        features_std = scaler.transform(features)
+        # 如果有PCA，应用PCA变换
+        if pca:
+            features_std = pca.transform(features_std)
+        # 进行预测
+        prediction = model.predict(features_std)
+        st.write(f"The predicted price is: ${prediction[0]:.2f}")
+    else:
+        st.write("Please enter valid values for all features.")
